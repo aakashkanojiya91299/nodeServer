7e95c336d53d478ab03fd8d499f23a7ca033c3f7
aakashwowrooms69 <103109534+aakashwowrooms69@users.noreply.github.com>
2022-04-11 19:21:28 +0530

    Add files via upload
    done till  ssr for no_lcc and lcc  remaining booking and ticketing and get-ticket details

--- server.js
+++ server.js
@@ -30,80 +30,102 @@ async function listen() {
 
 // Set the route for the root directory
 app.get('/api/search/TBO', search);
+app.get('/api/get_FareRule/TBO', get_FareRule);
 app.get('/api/getfare/TBO', getfare);
-app.get('/api/ssr_req/TBO',ssr_req);
-app.get('/api/booknow/TBO', booknow);
+app.get('/api/ssr_no_lcc/TBO', ssr_no_lcc);
+app.get('/api/ssr_lcc/TBO', ssr_lcc);
+app.get('/api/booknow_no_lcc/TBO', booknow_no_lcc);
+app.get('/api/booknow_lcc/TBO', booknow_lcc);
 app.get('/api/getTicket/TBO', getTicket);
 // This is what happens when any user requests '/'
-const job = schedule.scheduleJob({hour: 00, minute: 45}, (async function () {
-  try
-  {
+const job = schedule.scheduleJob({ hour: 00, minute: 45 }, (async function () {
+  try {
+    console.log("New ID----> ");
     await (Authenticate());
   }
-  catch (err)
-  {
+  catch (err) {
     console.log(err);
   }
 }));
-async function getTicket(req,res){
-  
+async function getTicket(req, res) {
+
 
 }
-async function booknow(req,res){
-  
+async function booknow_no_lcc(req, res) {
+
 
 }
+async function booknow_lcc(req, res) {
 
-async function ssr_req(req,res){
-  
 
 }
-// 
+async function get_FareRule(req,res){
+  getfare_data = req.body;
+  var get_FareRule = await get_FareRule_data(getfare_data);
+  res.send(get_FareRule);
+}
 async function getfare(req, res) {
-  console.log(req.body);
+  //console.log(req.body);
   getfare_data = req.body;
   //console.log();
   var get_FareRule = await get_FareRule_data(getfare_data);
   var get_FareQuote = await get_FareQuote_data(getfare_data);
+  //console.log("get_FareQuote",get_FareQuote);
   res.send(get_FareQuote);
+}
+async function ssr_no_lcc(req,res){
+  console.log(req.body);
+  ssr_no_lcc_data = req.body;
+  //console.log();
+  var ssr_no_lcc = await ssr_no_lcc_req(ssr_no_lcc_data);
+  res.send(ssr_no_lcc);
+
+}
+async function ssr_lcc(req,res){
+  //console.log("<------ssr_lcc----->");
+  ssr_lcc_data = req.body;
+  //console.log();
+  var ssr_lcc = await ssr_lcc_req(ssr_lcc_data);
+  res.send(ssr_lcc);
+
 }
 async function search(req, res) {
   // Just send back "Hello World!"
   // Later we'll see how we might send back JSON
   console.log(req.body)
   search_data = req.body;
-  
-  //Token creation 
-  try{
-    if (process.env.TokenId==0){
-    id = await Authenticate();
 
-  // console.log("after authenticate--->", id, typeof (id));
-  }
+  //Token creation 
+  try {
+    if (process.env.TokenId == 0) {
+      id = await Authenticate();
+
+      // console.log("after authenticate--->", id, typeof (id));
+    }
 
 
     //id = ;
-  if (process.env.TokenId != null) {
-    search_req = await search_result(search_data);
-    if (search_req != null) {
+    if (process.env.TokenId != null) {
+      search_req = await search_result(search_data);
+      if (search_req != null) {
 
-      res.send(search_req);
+        res.send(search_req);
+
+      }
+      else {
+        res.send("search problem time issue");
+      }
 
     }
     else {
-      res.send("search problem time issue");
+      res.send("Authenticate issue");
     }
 
-  }
-  else {
-    res.send("Authenticate issue");
-  }
 
-
-}catch(error){
-  console.log("something went wrong");
-  console.log(error);
-}
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+  }
 }
 //authenticate 
 async function Authenticate() {
@@ -177,7 +199,7 @@ async function get_FareRule_data(booking_data) {
       "ResultIndex": booking_data.ResultIndex
     })
 
-        console.log(res.data);
+    console.log("FareRule->",res.data);
     return (res.data);
   }
   catch (error) {
@@ -195,7 +217,7 @@ async function get_FareQuote_data(booking_data) {
       "ResultIndex": booking_data.ResultIndex
     })
 
-        console.log(res.data);
+    //console.log(res.data);
     return (res.data);
   }
   catch (error) {
@@ -203,19 +225,55 @@ async function get_FareQuote_data(booking_data) {
     return (null);
   }
 }
-async function ssr(ssr_req){
+async function ssr_no_lcc_req(ssr_req_no_lcc) {
+  const api_url = 'http://api.tektravels.com/BookingEngineService_Air/AirService.svc/rest/SSR';
+  try {
+    const res = await axios.post(api_url, {
+      "EndUserIp": "0.0.0.0",
+      "TokenId": process.env.TokenId,
+      "TraceId": ssr_req_no_lcc.TraceId,
+      "ResultIndex": ssr_req_no_lcc.ResultIndex
+    })
 
-}
-async function Booking_no_lcc(no_lcc_booking){
-
-}
-async function Ticket_lcc(Ticket_lcc_data){
-
-}
-async function Ticket_no_lcc(Ticket_no_lcc_data){
-
-}
-async function get_booking_details(booking_details){
+    console.log(res.data);
+    return (res.data);
+  }
+  catch (error) {
+    console.error("ssr error");
+    return (null);
+  }
   
+}
+async function ssr_lcc_req(ssr_lcc_data) {
+  console.log(ssr_lcc_data);
+  const api_url = 'http://api.tektravels.com/BookingEngineService_Air/AirService.svc/rest/SSR';
+  try {
+    const res = await axios.post(api_url, {
+      "EndUserIp": "0.0.0.0",
+      "TokenId": process.env.TokenId,
+      "TraceId": ssr_lcc_data.TraceId,
+      "ResultIndex": ssr_lcc_data.ResultIndex
+    })
+
+    console.log(res.data);
+    return (res.data);
+  }
+  catch (error) {
+    console.error("ssr error",error);
+    return (null);
+  }
+}
+
+async function Booking_no_lcc(no_lcc_booking) {
+
+}
+async function Ticket_lcc(Ticket_lcc_data) {
+
+}
+async function Ticket_no_lcc(Ticket_no_lcc_data) {
+
+}
+async function get_booking_details(booking_details) {
+
 }
 
